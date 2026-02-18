69f6e8bf90a7e0d89802b3c954ee7fd90028382e
vivekThoke <vivekthoke5@gmail.com>
2026-02-18 20:55:55 +0530

    Leave request model with approve, reject and ai anlysis added

---
+++ ExpenseManagement.Domain/Entities/LeaveRequest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpenseManagement.Domain.Enums;
+
+namespace ExpenseManagement.Domain.Entities
+{
+    public class LeaveRequest
+    {
+        public Guid Id { get; private set; }
+        public Guid UserId { get; private set; }
+        public LeaveType LeaveType { get; private set; }
+        public DateTime FromDate { get; private set; }
+        public DateTime ToDate { get; private set; }
+        public string Reason { get; private set; } = default!;
+        public LeaveStatus Status { get; private set; }
+        public int? RiskScore { get; private set; }
+        public string? AiSummary { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+
+        private LeaveRequest() { }
+
+        public LeaveRequest(Guid userId, LeaveType leaveType, DateTime formDate, DateTime toDate, string reason)
+        {
+            if (toDate < formDate)
+            {
+                throw new ArgumentException("To-Date cannot be earlier than Form-Date");
+            }
+
+            if (string.IsNullOrWhiteSpace(reason))
+            {
+                throw new ArgumentException("Reason is required");
+            }
+
+            Id = Guid.NewGuid();
+            UserId = userId;
+            LeaveType = leaveType;
+            FromDate = formDate;
+            ToDate = toDate;
+            Reason = reason;
+            Status = LeaveStatus.Pending;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public void Approve()
+        {
+            if(Status != LeaveStatus.Pending)
+            {
+                throw new InvalidOperationException("Only pending requests can be approved");
+            }
+
+            Status = LeaveStatus.Approved;
+        }
+
+        public void Reject()
+        {
+            if (Status != LeaveStatus.Pending)
+            {
+                throw new InvalidOperationException("Only pending request can be rejected");
+            }
+
+            Status = LeaveStatus.Rejected;
+        }
+
+        public void SetAiAnalysis(int riskScore, string aiSummary)
+        {
+            RiskScore = riskScore;
+            AiSummary = aiSummary;
+        }
+    }
+}
