a3d7927e7cf19bbf2e00e2a7500c3c2899f70150
vivekThoke <vivekthoke5@gmail.com>
2026-02-19 19:54:44 +0530

    added decision log

---
+++ ExpenseManagement.Domain/Entities/DecisionLog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpenseManagement.Domain.Enums;
+
+namespace ExpenseManagement.Domain.Entities
+{
+    public class DecisionLog
+    {
+        public Guid Id { get; private set; }
+        public Guid LeaveRequestId { get; private set; }
+        public int PolicyScore { get; private set; }
+        public int PatternScore { get; private set; }
+        public int AiScore { get; private set; }
+        public int FinalRiskScore { get; private set; }
+        public LeaveStatus ManagerDecision { get; private set; }
+        public string? OverrideReason { get; private set; }
+        public DateTime DecidedAt { get; private set; }
+
+        private DecisionLog() { }
+
+        public DecisionLog(Guid leaveRequestId, 
+            int policyScore, 
+            int patternScore, 
+            int aiScore, 
+            int finalRiskScore, 
+            LeaveStatus managerDecision, 
+            string? overrideReason = null)
+        {
+            Id = Guid.NewGuid();
+            LeaveRequestId = leaveRequestId;
+            PolicyScore = policyScore;
+            PatternScore = patternScore;
+            AiScore = aiScore;
+            FinalRiskScore = finalRiskScore;
+            ManagerDecision = managerDecision;
+            OverrideReason = overrideReason;
+            DecidedAt = DateTime.UtcNow;
+        }
+    }
+}
---
+++ ExpenseManagement.Domain/Entities/LeaveBalance.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpenseManagement.Domain.Enums;
+
+namespace ExpenseManagement.Domain.Entities
+{
+    public class LeaveBalance
+    {
+        public Guid UserId { get; private set; }
+        public LeaveType LeaveType { get; private set; }
+        public int RemainingDays { get; private set; }
+
+        private LeaveBalance() { }
+
+        public LeaveBalance(Guid userId, LeaveType leaveType, int initialDays)
+        {
+            if (initialDays < 0)
+            {
+                throw new ArgumentException("Initial days cannot be negative");
+            }
+
+            UserId = userId;
+            LeaveType = leaveType;
+            RemainingDays = initialDays;
+        }
+
+        public void DeductDays(int days)
+        {
+            if (days <= 0)
+            {
+                throw new ArgumentException("Days must be greater than zero");
+            }
+
+            if (RemainingDays < days)
+            {
+                throw new InvalidOperationException("Insufficient leave balance");
+            }
+
+            RemainingDays -= days;
+        }
+
+        public void AddDays(int days)
+        {
+            if (days <= 0)
+            {
+                throw new ArgumentException("Days must be greater than zero");
+            }
+
+            RemainingDays += days;
+        }
+
+    }
+}
