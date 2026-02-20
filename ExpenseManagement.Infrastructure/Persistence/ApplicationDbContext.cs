515a5bda640df01b9218403e5a009779cd5a3698
vivekThoke <vivekthoke5@gmail.com>
2026-02-20 22:23:00 +0530

    Added db context for other entity

---
+++ ExpenseManagement.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpenseManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseManagement.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<LeaveRequest> LeaveRequests => Set<LeaveRequest>();
+        public DbSet<LeaveBalance> LeaveBalances => Set<LeaveBalance>();
+        public DbSet<DecisionLog> decisionLogs => Set<DecisionLog>();
+    }
+}
