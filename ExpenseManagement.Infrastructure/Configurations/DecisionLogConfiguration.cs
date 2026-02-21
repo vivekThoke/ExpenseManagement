cf0802cf5949d931a4ed1801e80b9e7e9c6d8376
vivekThoke <vivekthoke5@gmail.com>
2026-02-21 11:31:55 +0530

    Database configuration for entity like decision log and Leave balance

---
+++ ExpenseManagement.Infrastructure/Configurations/DecisionLogConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpenseManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExpenseManagement.Infrastructure.Configurations
+{
+    public class DecisionLogConfiguration : IEntityTypeConfiguration<DecisionLog>
+    {
+        public void Configure(EntityTypeBuilder<DecisionLog> builder)
+        {
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.PolicyScore).IsRequired();
+            builder.Property(b => b.PatternScore).IsRequired();
+            builder.Property(b => b.AiScore).IsRequired();
+            builder.Property(b => b.FinalRiskScore).IsRequired();
+
+            builder.Property(b => b.DecidedAt).IsRequired();
+        }
+    }
+}
---
+++ ExpenseManagement.Infrastructure/Configurations/LeaveBalanceConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpenseManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExpenseManagement.Infrastructure.Configurations
+{
+    public class LeaveBalanceConfiguration : IEntityTypeConfiguration<LeaveBalance>
+    {
+        public void Configure(EntityTypeBuilder<LeaveBalance> builder)
+        {
+            builder.HasKey(lb => new { lb.UserId, lb.LeaveType });
+
+            builder.Property(lb => lb.RemainingDays).IsRequired();
+        } 
+    }
+}
