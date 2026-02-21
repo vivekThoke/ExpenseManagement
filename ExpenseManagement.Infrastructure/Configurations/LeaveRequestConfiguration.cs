c788559fb2b1b78edc231a46d780dcafcbf801bf
vivekThoke <vivekthoke5@gmail.com>
2026-02-21 11:18:37 +0530

    Configured entity for Leave request, user

---
+++ ExpenseManagement.Infrastructure/Configurations/LeaveRequestConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpenseManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExpenseManagement.Infrastructure.Configurations
+{
+    public class LeaveRequestConfiguration : IEntityTypeConfiguration<LeaveRequest>
+    {
+        public void Configure(EntityTypeBuilder<LeaveRequest> builder)
+        {
+            builder.HasKey(l => l.Id);
+
+            builder.Property(l => l.Reason).HasMaxLength(1000);
+
+            builder.Property(l => l.Status).IsRequired();
+
+            builder.Property(l => l.CreatedAt).IsRequired();
+
+            builder.Property(l => l.AiSummary).HasMaxLength(2000).IsRequired();
+
+            builder.Property(l => l.RiskScore).IsRequired(false);
+
+            builder.HasOne<User>().WithMany().HasForeignKey(l => l.UserId).OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ ExpenseManagement.Infrastructure/Configurations/UserConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpenseManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExpenseManagement.Infrastructure.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(150);
+
+            builder.Property(p => p.Email).IsRequired().HasMaxLength(200);
+
+            builder.HasIndex(i => i.Email).IsUnique();
+
+            builder.Property(p => p.Role).IsRequired();
+
+            builder.Property(p => p.ManagerId).IsRequired(false);
+        }
+    }
+}
