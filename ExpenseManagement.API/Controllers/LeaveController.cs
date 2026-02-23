86e034ea772a0592325b12a2af871fe13463c96d
vivekThoke <vivekthoke5@gmail.com>
2026-02-23 20:42:03 +0530

    Created API endpoint for leave request post method or submit

---
+++ ExpenseManagement.API/Controllers/LeaveController.cs
@@ -0,0 +1,27 @@
+﻿using ExpenseManagement.Application.DTOs;
+using ExpenseManagement.Application.Features.Leave;
+using ExpenseManagement.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace ExpenseManagement.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LeaveController : Controller
+    {
+        private readonly SubmitLeaveRequestService submitService;
+
+        public LeaveController(SubmitLeaveRequestService submitService)
+        {
+            this.submitService = submitService;
+        }
+
+        [HttpPost("submit")]
+        public async Task<IActionResult> Submit(SubmitLeaveRequestDto dto)
+        {
+            var id = await submitService.ExecuteAsync(dto);
+            return Ok(new { LeaveRequestId = id });
+        }
+    }
+}
