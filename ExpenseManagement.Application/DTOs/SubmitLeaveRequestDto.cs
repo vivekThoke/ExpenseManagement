1b671ed0303b08d2f28afe2e8f53be32fd5f36fb
vivekThoke <vivekthoke5@gmail.com>
2026-02-23 20:23:13 +0530

    Added submit request DTO

---
+++ ExpenseManagement.Application/DTOs/SubmitLeaveRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpenseManagement.Domain.Enums;
+
+namespace ExpenseManagement.Application.DTOs
+{
+    public class SubmitLeaveRequestDto
+    {
+        public Guid UserId { get; set; }
+        public LeaveType LeaveType { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public string Reason { get; set; } = string.Empty;
+
+    }
+}
