6f6b5fd34a56e2c583537a65537f30c768419b61
vivekThoke <vivekthoke5@gmail.com>
2026-02-23 20:35:46 +0530

    Added leave request services

--- ExpenseManagement.Application/Features/Leave/SubmitLeaveRequestService.cs
+++ ExpenseManagement.Application/Features/Leave/SubmitLeaveRequestService.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExpenseManagement.Application.DTOs;
+using ExpenseManagement.Domain.Entities;
+using ExpenseManagement.Domain.Interfaces;
 
 namespace ExpenseManagement.Application.Features.Leave
 {
-    internal class SubmitLeaveRequestService
+    public class SubmitLeaveRequestService
     {
+        private readonly ILeaveRepository leaveRepository;
+        private readonly ILeaveBalanceRepository leaveBalanceRepository;
+
+        public SubmitLeaveRequestService(ILeaveRepository leaveRepository, ILeaveBalanceRepository leaveBalanceRepository)
+        {
+            this.leaveRepository = leaveRepository;
+            this.leaveBalanceRepository = leaveBalanceRepository;
+        }
+
+        public async Task<Guid> ExecuteAsync(SubmitLeaveRequestDto dto)
+        {
+            var totalDays = (dto.ToDate - dto.FromDate).Days + 1;
+
+            var balance = await leaveBalanceRepository.GetAsync(dto.UserId, dto.LeaveType);
+
+            if (balance == null)
+                throw new Exception("Leave balance not found");
+
+            if (balance.RemainingDays < totalDays)
+                throw new Exception("Insufficient leave balance");
+
+            var leaveRequest = new LeaveRequest(dto.UserId, dto.LeaveType, dto.FromDate, dto.ToDate, dto.Reason);
+
+            await leaveRepository.AddAsync(leaveRequest);
+
+            return leaveRequest.Id;
+        }
     }
 }
