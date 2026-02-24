0c711f869139d1af765c40da6ba214600fc32489
vivekThoke <vivekthoke5@gmail.com>
2026-02-24 22:37:30 +0530

    Added leave request dto

---
+++ ExpenseManagement.Application/DTOs/ApproveLeaveRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseManagement.Application.DTOs
+{
+    public class ApproveLeaveRequestDto
+    {
+        public Guid LeaveRequestId { get; set; }
+        public bool Approve { get; set; }
+    }
+}
